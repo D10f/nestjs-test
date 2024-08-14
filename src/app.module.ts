8a904ff3b5cf6d6efbe3a9597caac13dfc94bb33
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-08-14 23:44:46 +0200

    Refactor app.module to move module configuration objects inline for better readability

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,56 +1,48 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule, MongooseModuleAsyncOptions } from '@nestjs/mongoose';
-import {
-  ConfigModule,
-  ConfigService,
-  ConfigModuleOptions,
-} from '@nestjs/config';
-import { JwtModule, JwtModuleAsyncOptions } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AppConfig, validationSchema } from './config/schema';
 
-const mongooseConfig: MongooseModuleAsyncOptions = {
-  imports: [ConfigModule],
-  inject: [ConfigService],
-  useFactory: (configService: ConfigService<AppConfig>) => {
-    const name = configService.get('MONGODB_NAME');
-    const user = configService.get('MONGODB_USER');
-    const pass = configService.get('MONGODB_PASSWORD');
-
-    return {
-      uri: `mongodb://${user}:${pass}@mongo:27017/${name}?authSource=admin`,
-    };
-  },
-};
-
-const envConfig: ConfigModuleOptions = {
-  ignoreEnvFile: true,
-  validate: validationSchema.parse,
-  isGlobal: true,
-};
-
-const jwtConfig: JwtModuleAsyncOptions = {
-  imports: [ConfigModule],
-  inject: [ConfigService],
-  global: true,
-  useFactory: (configService: ConfigService<AppConfig>) => ({
-    secret: configService.get('JWT_ACCESS_SECRET'),
-    signOptions: {
-      expiresIn: configService.get('JWT_ACCESS_EXPIRES'),
-    },
-  }),
-};
-
 @Module({
   imports: [
     UserModule,
     AuthModule,
-    MongooseModule.forRootAsync(mongooseConfig),
-    ConfigModule.forRoot(envConfig),
-    JwtModule.registerAsync(jwtConfig),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<AppConfig>) => {
+        const name = configService.get('MONGODB_NAME');
+        const user = configService.get('MONGODB_USER');
+        const pass = configService.get('MONGODB_PASSWORD');
+
+        return {
+          uri: `mongodb://${user}:${pass}@mongo:27017/${name}?authSource=admin`,
+        };
+      },
+    }),
+
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      validate: validationSchema.parse,
+      isGlobal: true,
+    }),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      global: true,
+      useFactory: (configService: ConfigService<AppConfig>) => ({
+        secret: configService.get('JWT_ACCESS_SECRET'),
+        signOptions: {
+          expiresIn: configService.get('JWT_ACCESS_EXPIRES'),
+        },
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
