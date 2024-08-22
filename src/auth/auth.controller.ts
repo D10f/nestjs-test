6f9c0614c171912b810459eb42e8715dce8f839e
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-08-22 15:04:20 +0200

    Apply exception filters and guards on entry point and Auth controller. Added cookie-parser.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,49 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Headers,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { AuthGuard } from './guards/jwt.guard';
+import { AuthRefreshGuard } from './guards/jwt-refresh.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() createUserDto: CreateUserDto) {
-    return this.authService.signup(createUserDto);
+  signup(
+    @Body() createUserDto: CreateUserDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    return this.authService.signup(createUserDto, response);
   }
 
   @Post('login')
-  login(@Body() createUserDto: Partial<CreateUserDto>) {
-    return this.authService.login(createUserDto);
+  login(
+    @Body() createUserDto: Partial<CreateUserDto>,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    return this.authService.login(createUserDto, response);
+  }
+
+  @Post('logout')
+  @UseGuards(AuthGuard)
+  logout(@Res({ passthrough: true }) res: Response) {
+    return this.authService.logout(res);
+  }
+
+  @Post('refresh')
+  @UseGuards(AuthRefreshGuard)
+  refresh(
+    @Headers('cookie') cookies: string,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    return this.authService.refresh(cookies, res);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { AppConfig } from './config/schema';
+import { JwtExceptionFilter } from './filters/jwt.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,10 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+
+  app.useGlobalFilters(new JwtExceptionFilter());
+
+  app.use(cookieParser());
 
   await app.listen(config.get('PORT'));
   console.log(`app running in port: ${config.get('PORT')}`);
