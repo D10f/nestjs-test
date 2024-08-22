e87829f48cdfc9ee1995a670c8add5d9c38ef45a
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-08-22 18:47:59 +0200

    Add decorators to fetch cookies from request headers. Add decorator to fetch user from incoming request.

---
+++ src/decorators/cookie.decorator.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
+
+export const Cookie = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<Request>();
+    const cookies = request.headers.cookie;
+    const re = new RegExp(`${data}=[^;]+`);
+    const match = cookies.match(re);
+    return match ? match[0].split('=')[1] : null;
+  },
+);
