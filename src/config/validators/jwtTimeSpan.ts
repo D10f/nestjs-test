b0fa344f92b3187360de02901150c84aab1ce18b
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-09-18 22:32:41 +0200

    Updated zod validation for JWT expiration time, using custom made time parser. Return value to the config module is now milliseconds as opposed to original time span string, so this can be reused in other areas such as cookie maxAge property

--- src/config/validators/jwtTimeSpan.ts
+++ src/config/validators/jwtTimeSpan.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import parseTime from '@d10f/time-utils';
 
 /**
  * The JWT library supports both numbers or strings but this function enforces
@@ -6,30 +7,34 @@ import { z } from 'zod';
  * and helps avoid misunderstandings.
  */
 export function isValidTimeSpan(val: string, ctx: z.RefinementCtx) {
-  const match = val.match(
-    /^(?<value>-?(?:\d+)?\.?\d+) *(?<type>milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|weeks?|w|years?|yrs?|y)?$/i,
-  );
+  //const match = val.match(
+  //  /^(?<value>-?(?:\d+)?\.?\d+) *(?<type>milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|weeks?|w|years?|yrs?|y)?$/i,
+  //);
 
-  if (!match) {
-    ctx.addIssue({
-      code: z.ZodIssueCode.invalid_string,
-      validation: 'regex',
-      message:
-        'JWT expiration time must be expressed as an time amount e.g. "20d", "20 days", "15m", "15 minutes", "1w" or "1 week"',
-    });
-    return z.NEVER;
+  try {
+    const resultInMs = parseTime(val);
+
+    if (resultInMs <= 0) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.too_small,
+        minimum: 1,
+        inclusive: true,
+        type: 'number',
+        message: 'Expiration time must be a positive integer',
+      });
+      return z.NEVER;
+    }
+
+    return resultInMs;
+  } catch (error) {
+    if ((error as Error).message === 'Invalid input.') {
+      ctx.addIssue({
+        code: z.ZodIssueCode.invalid_string,
+        validation: 'regex',
+        message:
+          'JWT expiration time must be expressed as an time amount e.g. "20d", "20 days", "15m", "15 minutes", "1w" or "1 week"',
+      });
+      return z.NEVER;
+    }
   }
-
-  if (parseInt(match.groups.value) <= 0) {
-    ctx.addIssue({
-      code: z.ZodIssueCode.too_small,
-      minimum: 1,
-      inclusive: true,
-      type: 'number',
-      message: 'Expiration time must be a positive integer',
-    });
-    return z.NEVER;
-  }
-
-  return val;
 }
